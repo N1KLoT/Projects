86eca34098a75d3aeca9d8bde14375740dc662bc
Thomas Knell <knell.thomas@gmail.com>
2014-07-14 16:13:33 +0200

    reverse string done

---
+++ Text/reverse_a_string.py
@@ -0,0 +1,15 @@
+'''
+Created on Jul 14, 2014
+
+@author: thomasknell
+'''
+
+def main():
+    input_string = raw_input("string to reverse: ")
+    reversed_string = ""
+    for i in xrange(1, len(input_string) + 1):
+        reversed_string += input_string[len(input_string) - i]
+    print(input_string + " revered is " + reversed_string)
+
+if __name__ == "__main__":
+    main()
