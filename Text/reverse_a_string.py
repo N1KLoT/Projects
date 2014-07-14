c5141f44c95b7d0973b0f04f6549f6b0272f6071
Thomas Knell <knell.thomas@gmail.com>
2014-07-14 19:13:53 +0200

    fixed for python 3

--- Text/reverse_a_string.py
+++ Text/reverse_a_string.py
@@ -5,9 +5,9 @@ Created on Jul 14, 2014
 '''
 
 def main():
-    input_string = raw_input("string to reverse: ")
+    input_string = input("string to reverse: ")
     reversed_string = ""
-    for i in xrange(1, len(input_string) + 1):
+    for i in range(1, len(input_string) + 1):
         reversed_string += input_string[len(input_string) - i]
     print(input_string + " revered is " + reversed_string)
 
